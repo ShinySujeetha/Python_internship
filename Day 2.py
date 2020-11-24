d78e12e5375d92dacd28883f00d2d10ebd4ed714
Shiny Sujeetha <74914729+ShinySujeetha@users.noreply.github.com>
2020-11-24 17:28:14 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,12 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b,c=100,10,30
+>>> print(a/10,b*50,c+60)
+10.0 500 90
+>>> str='SHINY'
+>>> print(str.replace('I','G'))
+SHGNY
+>>> a,b=6,8.736
+>>> print(float(a),int(b))
+6.0 8
+>>> 
